c7d89fd572d32de5f4d5ad49f49099445e4771b2
septiacahyanir <septiacahyani81@gmail.com>
2024-11-27 15:33:11 +0700

    Add files via upload

---
+++ 008_Septia cahyani ramadhani luas persegi.py
@@ -0,0 +1,7 @@
+def luas_persegi_panjang(Panjang, Lebar):
+    return Panjang * Lebar   
+ 
+panjang = float(input("Masukkan panjang: ")) 
+lebar = float(input("Masukkan lebar: "))
+
+print("Luas persegi panjang adalah:", luas_persegi_panjang(panjang, lebar))
